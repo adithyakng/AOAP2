e9139a463e59cd76b5163b27b1b7c0f95f897b1f
Adithya KNG <adithyakng@gmail.com>
2023-04-02 13:36:55 -0400

    Task5A.java

--- Task5A.java
+++ Task5A.java
@@ -53,48 +53,56 @@ public class Task5A {
         // Now construct the DP matrix in Top Down manner using recursion
         int dp[][] = new int[m][n];
 
-        // For memoization, fill the dp matrix with -1
+        // For memoization, fill the dp matrix with 0
         for(int i=0; i<m;i++){
-            Arrays.fill(dp[i],-1);
+            Arrays.fill(dp[i],0);
         }
         int ans[] = new int[4];
-        int maxSize = calculateMaxArea(valid, dp, m-1, n-1, ans, 0, plot, h);
+        calculateMaxArea(valid, dp, m-1, n-1,plot, h);
+
+        // Now we have the DP array populated, iterate through it and findMaxSize and the correspoinding top left and bottom right coordinates
+        int maxSize = -1;
+        for(int i=0; i<m;i++){
+            for(int j=0;j<n; j++){
+
+                // If only 1 row or 1 column is present then the first element is the answer
+                if( (i == 0 || j ==0) && maxSize == -1){
+                    maxSize = 1;
+                    ans[0] = ans[1] = ans[2] = ans[3] = i+1;
+                }
+
+                else if(dp[i][j] > maxSize){
+                    maxSize = dp[i][j];
+                    ans[0] = i-(maxSize-1)+1;  // x1
+                    ans[1] = i+1; //x2
+                    ans[2] = j-(maxSize-1)+1; //y1
+                    ans[3] = j+1; //y2
+                }
+            }
+        }
         System.out.println(ans[0] + " " + ans[2] + " " + ans[1] + " " + ans[3]);
 
 
     }
 
-    public static int calculateMaxArea(int valid[][],int dp[][], int i, int j, int ans[],int maxSize,int plot[][],int h){
+    public static int calculateMaxArea(int valid[][],int dp[][], int i, int j,int plot[][],int h){
         if(i <0 || j<0){
             return 0;
         }
         if( i == 0 || j ==0 ){
-            maxSize = Math.max(maxSize,1);
-            ans[0] = ans[1] = ans[2] = ans[3] = i+1;
+            // If the plot is in the first row or first coloumn then it is a plots will act as a corner plot and the area will be 1
+            dp[i][j] = 1;
             return 1;
         }
-        else if((i == 1 && j>=1) || (j==1 && i>=1)){
-            // If the plot is in the second row or second coloumn then all 4 plots will act as a corner plot and the area will be 2
-            maxSize = Math.max(maxSize,2);
-            ans[0] = 1;
-            ans[1] = 2;
-            ans[2] = 1;
-            ans[3] = 2;
-            return 2;
-           
-        }
 
-        if(dp[i-1][j-1] == -1) {
-            dp[i-1][j-1] = calculateMaxArea(valid,dp,i-1,j-1,ans,maxSize, plot, h);
-            maxSize = Math.max(maxSize,dp[i-1][j-1]);
+        if(dp[i-1][j-1] == 0) {
+            dp[i-1][j-1] = calculateMaxArea(valid,dp,i-1,j-1,plot, h);
         }
-        if(dp[i-1][j] == -1) {
-            dp[i-1][j] = calculateMaxArea(valid, dp, i-1, j, ans, maxSize, plot, h);
-            maxSize = Math.max(maxSize,dp[i-1][j]);
+        if(dp[i-1][j] == 0) {
+            dp[i-1][j] = calculateMaxArea(valid, dp, i-1, j,plot, h);
         }
-        if(dp[i][j-1] == -1){
-            dp[i][j-1] = calculateMaxArea(valid, dp, i, j-1, ans, maxSize, plot, h);
-            maxSize = Math.max(maxSize,dp[i][j-1]);
+        if(dp[i][j-1] == 0){
+            dp[i][j-1] = calculateMaxArea(valid, dp, i, j-1,plot, h);
         }
         int min = getMinimum(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]);
         int validPlotCount = getValidCount(i+1,j+1,min, valid);
@@ -125,15 +133,6 @@ public class Task5A {
                 return 2;
             }
             dp[i][j] = min+1;
-            // All plots within the bounding square (i,j) to (i+k,j+k) now have values >=h except for the corner plots, 
-            // so check maxArea and update coordinates
-            if((min+1) > maxSize){
-                maxSize = (min+1);
-                ans[0] = (i-min+1); //x1
-                ans[1] = (i+1); //x2
-                ans[2] = (j+1-min); // y1
-                ans[3] = (j+1); // y2
-            }
             return dp[i][j];
         }
 
